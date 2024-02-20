61e937523aa5f34e98ea89585285ec817fbe67e6
vaibhavtiwari0112 <vaibhavtiwari0128@gmail.com>
2024-02-21 00:48:30 +0530

    dd

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+export default Profile = () => {
+  return <div>Profile</div>;
+};
