112365e94b1ae1f7ee283f5938f72a4d2482604e
vaibhavtiwari0112 <vaibhavtiwari0128@gmail.com>
2024-02-21 00:40:30 +0530

    first commit of client creation

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignOut from "./pages/SignOut";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
