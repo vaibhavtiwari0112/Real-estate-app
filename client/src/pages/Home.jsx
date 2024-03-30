0c19583e4e61d8ea22953d78eaf738f31bde82af
vaibhavtiwari0112 <vaibhavtiwari0128@gmail.com>
2024-03-30 14:30:21 +0530

    updated Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -47,15 +47,15 @@ export default function Home() {
   }, []);
   return (
     <div>
-      {/* top */}
+
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span>
+         Check Amazing<span className="text-slate-500">Amazing</span>
           <br />
-          place with ease
+          place with Amazing E-State
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Sahand Estate is the best place to find your next perfect place to
+          Amazing Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
@@ -64,7 +64,7 @@ export default function Home() {
           to={"/search"}
           className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
         >
-          Let's get started...
+          Let's get started...😀
         </Link>
       </div>
 
@@ -86,7 +86,6 @@ export default function Home() {
           ))}
       </Swiper>
 
-      {/* listing results for offer, sale and rent */}
 
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
